660d73d374da67eb338a603e2c9d0966e96a7513
iamdudeman <timothy.solum@gmail.com>
2022-06-07 10:49:50 -0500

    added EcsView6 and updated Javadocs

--- src/main/java/technology/sola/ecs/view/EcsView2.java
+++ src/main/java/technology/sola/ecs/view/EcsView2.java
@@ -3,5 +3,12 @@ package technology.sola.ecs.view;
 import technology.sola.ecs.Component;
 import technology.sola.ecs.Entity;
 
+/**
+ * Contains two {@link Component}s that were searched for.
+ *
+ * @param entity the {@link Entity}
+ * @param c1 the first component
+ * @param c2 the second component
+ */
 public record EcsView2<C1 extends Component, C2 extends Component>(Entity entity, C1 c1, C2 c2) {
 }
--- src/main/java/technology/sola/ecs/view/EcsView6.java
+++ src/main/java/technology/sola/ecs/view/EcsView6.java
@@ -4,7 +4,7 @@ import technology.sola.ecs.Component;
 import technology.sola.ecs.Entity;
 
 /**
- * Contains five {@link Component}s that were searched for.
+ * Contains six {@link Component}s that were searched for.
  *
  * @param entity the {@link Entity}
  * @param c1 the first component
@@ -12,8 +12,9 @@ import technology.sola.ecs.Entity;
  * @param c3 the third component
  * @param c4 the fourth component
  * @param c5 the fifth component
+ * @param c6 the sixth component
  */
-public record EcsView5<C1 extends Component, C2 extends Component, C3 extends Component, C4 extends Component, C5 extends Component> (
-  Entity entity, C1 c1, C2 c2, C3 c3, C4 c4, C5 c5
+public record EcsView6<C1 extends Component, C2 extends Component, C3 extends Component, C4 extends Component, C5 extends Component, C6 extends Component> (
+  Entity entity, C1 c1, C2 c2, C3 c3, C4 c4, C5 c5, C6 c6
 ) {
 }
