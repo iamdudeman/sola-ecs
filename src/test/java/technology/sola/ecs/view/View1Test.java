0c1f676916ee31af8c7067ea23c7aa0b8c27723d
iamdudeman <timothy.solum@gmail.com>
2023-02-13 18:39:17 -0600

    Added ViewN tests

---
+++ src/test/java/technology/sola/ecs/view/View1Test.java
@@ -0,0 +1,47 @@
+package technology.sola.ecs.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import technology.sola.ecs.Entity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class View1Test extends ViewTestBase {
+  private View1<TestComponent> view;
+
+  @BeforeEach
+  void setup() {
+    view = new View1<>(TestComponent.class);
+  }
+
+  @Nested
+  class createEntryFromEntity {
+    @Test
+    void whenAllComponentsPresent_shouldReturnEntity() {
+      Entity mockEntity = createMockEntity(
+        new ComponentMapping<>(TestComponent.class, testComponent)
+      );
+
+      var entry = view.createEntryFromEntity(mockEntity);
+
+      assertNotNull(entry);
+      assertEquals(testComponent, entry.c1());
+    }
+
+    @Test
+    void whenComponentMissing_shouldReturnNull() {
+      var missingCombinations = List.of(
+        createMockEntity()
+      );
+
+      missingCombinations.forEach(mockEntity -> {
+        var entry = view.createEntryFromEntity(mockEntity);
+
+        assertNull(entry);
+      });
+    }
+  }
+}
---
+++ src/test/java/technology/sola/ecs/view/ViewTestBase.java
@@ -0,0 +1,35 @@
+package technology.sola.ecs.view;
+
+import org.mockito.Mockito;
+import technology.sola.ecs.Component;
+import technology.sola.ecs.Entity;
+
+abstract class ViewTestBase {
+  protected final TestComponent testComponent = new TestComponent();
+  protected final TestComponent2 testComponent2 = new TestComponent2();
+  protected final TestComponent3 testComponent3 = new TestComponent3();
+
+  protected Entity createMockEntity(ComponentMapping<?>... componentMappings) {
+    Entity mockEntity = Mockito.mock(Entity.class);
+
+    if (componentMappings != null) {
+      for (var mapping : componentMappings) {
+        Mockito.doReturn(mapping.instance).when(mockEntity).getComponent(mapping.clazz);
+      }
+    }
+
+    return mockEntity;
+  }
+
+  protected record ComponentMapping<T extends Component>(Class<T> clazz, T instance) {
+  }
+
+  protected record TestComponent() implements Component {
+  }
+
+  protected record TestComponent2() implements Component {
+  }
+
+  protected record TestComponent3() implements Component {
+  }
+}
