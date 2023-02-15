ac3d84a9398e7b697a6433f628e74d24f09f74fa
iamdudeman <timothy.solum@gmail.com>
2023-02-15 10:40:50 -0600

    Added start to a full view integration test

---
+++ src/test/java/technology/sola/ecs/ViewIntegrationTest.java
@@ -0,0 +1,102 @@
+package technology.sola.ecs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import technology.sola.ecs.view.View1;
+import technology.sola.ecs.view.View2;
+import technology.sola.ecs.view.View3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ViewIntegrationTest {
+  private World testWorld;
+  private View1<TestComponent> testComponentView1;
+  private View2<TestComponent, TestComponent2> testComponentView12;
+  private View3<TestComponent, TestComponent2, TestComponent3> testComponentView123;
+
+  private Entity entityWithNoComponents;
+  private Entity entityWithTestComponent;
+  private Entity entityWithTestComponent12;
+  private Entity entityWithTestComponent123;
+
+  @BeforeEach
+  void setup() {
+    testWorld = new World(10);
+
+    entityWithTestComponent123 = testWorld.createEntity(
+      new TestComponent(), new TestComponent2(), new TestComponent3()
+    );
+    entityWithTestComponent12 = testWorld.createEntity(
+      new TestComponent(), new TestComponent2()
+    );
+    testWorld.createEntity(
+      new TestComponent(), new TestComponent3()
+    );
+    testWorld.createEntity(
+      new TestComponent2(), new TestComponent3()
+    );
+    entityWithTestComponent = testWorld.createEntity(
+      new TestComponent()
+    );
+    entityWithNoComponents = testWorld.createEntity();
+
+    testComponentView1 = testWorld.createView().of(TestComponent.class);
+    testComponentView12 = testWorld.createView().of(TestComponent.class, TestComponent2.class);
+    testComponentView123 = testWorld.createView().of(TestComponent.class, TestComponent2.class, TestComponent3.class);
+  }
+
+  @Nested
+  class createView {
+    @Test
+    void whenCalledTwice_shouldReturnSameInstance() {
+      assertEquals(testComponentView1, testWorld.createView().of(TestComponent.class));
+      assertEquals(testComponentView12, testWorld.createView().of(TestComponent.class, TestComponent2.class));
+      assertEquals(testComponentView123, testWorld.createView().of(TestComponent.class, TestComponent2.class, TestComponent3.class));
+    }
+
+    @Test
+    void shouldInitializeViews() {
+      assertEquals(4, testComponentView1.getEntries().size());
+      assertEquals(2, testComponentView12.getEntries().size());
+      assertEquals(1, testComponentView123.getEntries().size());
+    }
+  }
+
+  @Test
+  void updateForAddComponent_integrationTest() {
+    entityWithNoComponents.addComponent(new TestComponent());
+    assertEquals(5, testComponentView1.getEntries().size());
+    assertEquals(2, testComponentView12.getEntries().size());
+    assertEquals(1, testComponentView123.getEntries().size());
+
+    entityWithNoComponents.addComponent(new TestComponent2());
+    assertEquals(5, testComponentView1.getEntries().size());
+    assertEquals(3, testComponentView12.getEntries().size());
+    assertEquals(1, testComponentView123.getEntries().size());
+
+    entityWithTestComponent12.addComponent(new TestComponent3());
+    assertEquals(5, testComponentView1.getEntries().size());
+    assertEquals(3, testComponentView12.getEntries().size());
+    assertEquals(2, testComponentView123.getEntries().size());
+  }
+
+  @Test
+  void updateForRemoveComponent_integrationTest() {
+    // todo
+  }
+
+  @Test
+  void updateForDeletedEntity_integrationTest() {
+    // todo
+  }
+
+  private record TestComponent() implements Component {
+  }
+
+  private record TestComponent2() implements Component {
+  }
+
+  private record TestComponent3() implements Component {
+  }
+}
