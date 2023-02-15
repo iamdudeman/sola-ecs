e6b968ccec7ac15f2e9ec3133a878d3687e1ffcf
iamdudeman <timothy.solum@gmail.com>
2023-02-15 16:37:47 -0600

    added placeholders for ViewCacheTest

--- src/test/java/technology/sola/ecs/cache/ViewCacheTest.java
+++ src/test/java/technology/sola/ecs/cache/ViewCacheTest.java
@@ -1,7 +1,71 @@
 package technology.sola.ecs.cache;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import technology.sola.ecs.Component;
+import technology.sola.ecs.Entity;
+import technology.sola.ecs.World;
+import technology.sola.ecs.view.ViewEntry;
+
+import java.util.List;
+
 class ViewCacheTest {
-  // todo
+  private ViewCache viewCache;
 
+  @BeforeEach
+  void setup() {
+    World mockWorld = Mockito.mock(World.class);
 
+    viewCache = new ViewCache(mockWorld);
+  }
+
+  @Nested
+  class createView {
+    @Test
+    void whenCalledTwice_shouldReturnSameInstance() {
+      // todo
+    }
+
+    @Test
+    void shouldInitializeViews() {
+      // todo
+    }
+  }
+
+  @Test
+  void updateForAddComponent_shouldUpdateEachView() {
+    // todo
+  }
+
+  @Test
+  void updateCacheForRemoveComponent_shouldUpdateEachView() {
+    // todo
+  }
+
+  @Test
+  void updateCacheForDeletedEntity_shouldUpdateEachView() {
+    // todo
+  }
+
+  private static class TestView extends View<TestViewEntry> {
+    public TestView() {
+      super(List.of(TestComponent.class, TestComponent2.class));
+    }
+
+    @Override
+    protected TestViewEntry createEntryFromEntity(Entity entity) {
+      return null;
+    }
+  }
+
+  private record TestViewEntry(Entity entity) implements ViewEntry {
+  }
+
+  private record TestComponent() implements Component {
+  }
+
+  private record TestComponent2() implements Component {
+  }
 }
