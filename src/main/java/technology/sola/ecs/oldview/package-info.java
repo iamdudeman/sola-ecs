e667558f891e9bb604b5cf883df11e91e6fd9f99
iamdudeman <timothy.solum@gmail.com>
2023-02-16 16:26:50 -0600

    Removed old view code

--- src/main/java/technology/sola/ecs/oldview/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package provides view classes for usage by {@link technology.sola.ecs.SolaEcs} when querying a {@link technology.sola.ecs.World}.
- */
-package technology.sola.ecs.oldview;
